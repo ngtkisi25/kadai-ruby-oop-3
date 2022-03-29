06095af66182f471abd9d6d573c8ae37e59d0895
ngtkisi25 <manchester.c.fc21@gmail.com>
2022-03-29 13:49:48 +0000

    main

--- main.rb
+++ main.rb
@@ -1,6 +1,4 @@
-require './animal'
 require './human'
-require './thinkable'
 
 # インスタンスの作成
 tanaka = Human.new('田中 太郎', 25, '電車')
@@ -11,6 +9,6 @@ suzuki = Human.new('鈴木 次郎', 30, '野球')
 suzuki.say
 suzuki.think
 
-sato   = Human.new('佐藤 花子', 20, '映画')
+sato = Human.new('佐藤 花子', 20, '映画')
 sato.say
 sato.think
